0ce7392c91fd05273c2c43c4d047f3fe921d4b9e
beliaievandrii-stack <beliaiev.andrii@gmail.com>
2026-02-19 13:26:42 +0100

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,21 @@
+package lesson2;
+
+public class Variables {
+    // Главный метод
+    public static void main(String[] args) {
+        byte ErsteZahl = 43;
+        System.out.println(ErsteZahl);
+
+        int ZweiteZahl = 100000;
+        System.out.println(ZweiteZahl);
+
+        float Temperatura = 36.6000000000000000000999999999999f;
+        System.out.println(Temperatura);
+
+        double Temperatura2 = 36.69999000000000000000000099999999f;
+        System.out.println(Temperatura2);
+
+        long DriteZahl = ErsteZahl + ZweiteZahl;
+        System.out.println(DriteZahl);
+    }
+}
